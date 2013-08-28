4ede68ebe1782b77bb203f1e09054bb4dbb0cf6e
Zach Tirrell <zbtirrell@gmail.com>
2013-08-28 08:34:46 -0400

    updating comment header block

--- go-copylayout.php
+++ go-copylayout.php
@@ -1,15 +1,15 @@
 <?php
-/*
-Plugin Name: GigaOM Copy Layout
-Plugin URI: http://
-Description: Copy widgets and widget settings
-Version: 2.0
-Author: GigaOM
-Author URI: http://gigaom.com
-License: GPL2
-
-Notes: Based on the version developed by Adam Backstrom of Plymouth State University (http://www.plymouth.edu)
-*/
+/**
+ * Plugin Name: Gigaom Copy Layout
+ * Plugin URI: http://
+ * Description: Copy widgets and widget settings
+ * Version: 2.0
+ * Author: Gigaom
+ * Author URI: http://gigaom.com
+ * License: GPL2
+ *
+ * Notes: Based on the version developed by Adam Backstrom of Plymouth State University (http://www.plymouth.edu)
+ */
 
 require_once __DIR__ . '/components/class-go-copylayout.php';
 
